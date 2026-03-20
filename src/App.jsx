4bf5260e53e2ea38c7e8ac449b26edbae98ae9d3
hoiregeno <hoiregeno75@gmail.com>
2026-03-20 14:46:44 +1000

    Revert "feat: base layout and dark navbar"
    This reverts commit d34f9b6dcc03f0a135ed72af3490e1c2de56b9f6.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 function App() {
-  return (
-    <div className="bg-teal-950 min-h-screen">
-      <nav className="py-4 px-6 bg-teal-900 border-b border-teal-800">
-        <h1 className="text-2xl font-bold text-white">Ledgr</h1>
-      </nav>
-      <main></main>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
