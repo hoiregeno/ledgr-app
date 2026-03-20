8888505bcac15153dc6bf1a7b810e2589592b7c9
hoiregeno <hoiregeno75@gmail.com>
2026-03-20 17:17:13 +1000

    feat: add Layout component with sidebar and configure nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Debts from "./pages/Debts";
 import Expenses from "./pages/Expenses";
 import Inventory from "./pages/Inventory";
 import Sales from "./pages/Sales";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/debt" element={<Debts />} />
-        <Route path="/expenses" element={<Expenses />} />
-        <Route path="/inventory" element={<Inventory />} />
-        <Route path="/sales" element={<Sales />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="debts" element={<Debts />} />
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="inventory" element={<Inventory />} />
+          <Route path="sales" element={<Sales />} />
+        </Route>
       </Routes>
     </div>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,34 @@
+import { NavLink, Outlet } from "react-router-dom";
+
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <div>
+      <nav>
+        <h1>Ledgr</h1>
+        <ul>
+          <li>
+            <NavLink to="/dashboard">Dashboard</NavLink>
+          </li>
+          <li>
+            <NavLink to="/sales">Sales</NavLink>
+          </li>
+          <li>
+            <NavLink to="/expenses">Expenses</NavLink>
+          </li>
+          <li>
+            <NavLink to="/debts">Debts</NavLink>
+          </li>
+          <li>
+            <NavLink to="/inventory">Inventory</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default Layout;
