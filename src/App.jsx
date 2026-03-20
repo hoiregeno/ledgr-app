bb82463b6730494e9eaf93847e44b7171004e548
hoiregeno <hoiregeno75@gmail.com>
2026-03-20 16:58:43 +1000

    feat: configure BrowserRouter and define app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Debts from "./pages/Debts";
+import Expenses from "./pages/Expenses";
+import Inventory from "./pages/Inventory";
+import Sales from "./pages/Sales";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/debt" element={<Debts />} />
+        <Route path="/expenses" element={<Expenses />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/sales" element={<Sales />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
